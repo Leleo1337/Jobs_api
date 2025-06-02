f5b08d2e72e0d56dec4353c51647eb9ceec91edd
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-02 17:31:51 -0300

    added routes, middlewares, removed dotenv from app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
-import express from 'express'
-import { configDotenv } from 'dotenv'
+import express from "express";
+import notFoundMiddleware from "./middlewares/not-found.js";
+import errorHandlerMiddleware from "./middlewares/error-handler.js";
+import auth from "./routes/auth.js";
+import jobs from "./routes/jobs.js";
 
-configDotenv()
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/jobs", jobs);
 
-app.get('/', (req,res) => {
-    res.send('teste')
-})
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-export default app
+export default app;
