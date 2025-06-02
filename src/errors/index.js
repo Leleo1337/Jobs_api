6de026cb9c52fa334aa9e460b8066b9d6ad96baa
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-02 17:31:12 -0300

    create custom errors handlers
    - bad-request
    - not-found
    - unauthenticated
    ---
    - custom-api
    - index ( imports )

---
+++ src/errors/index.js
@@ -0,0 +1,6 @@
+import BadRequest from "./bad-request.js";
+import CustomApiError from "./custom-api.js";
+import NotFound from "./not-found.js";
+import Unauthenticated from "./unauthenticated.js";
+
+export default { CustomApiError, NotFound, Unauthenticated, BadRequest };
