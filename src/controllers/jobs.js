f725b9b9173e1a7459812bb865c56df3b3d0d653
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-02 17:32:04 -0300

    create jobs controller placeholder

---
+++ src/controllers/jobs.js
@@ -0,0 +1,18 @@
+export async function getAllJobs(req, res) {
+   res.send("get all jobs");
+}
+
+export async function getJob(req, res) {
+   res.send("get job");
+}
+export async function createJob(req, res) {
+   res.send("create job");
+}
+
+export async function updateJob(req, res) {
+   res.send("update job");
+}
+
+export async function deleteJob(req, res) {
+   res.send("delete job");
+}
