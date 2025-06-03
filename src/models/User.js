b83801ebab18d8bc39a5717ef407c59a50858a2a
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-03 19:56:07 -0300

    added middlewares in user schema
    - bcrypt in pre("save") middlware
    - create token method
    - compare password method
    
    Estou comentando o código com o objetivo de reforçar meu entendimento.

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+// schema do mongodb
 const UserSchema = new mongoose.Schema({
    name: {
       type: String,
@@ -24,10 +26,23 @@ const UserSchema = new mongoose.Schema({
    },
 });
 
-UserSchema.pre("save", async function (next) {
+// o pre("save") é um middleware que serve para executar uma função antes que algo seja salvo no banco de dados )
+// aqui estou usando para criptografar a senha antes que seja salva do no banco de dados
+UserSchema.pre("save", async function () {
    const salt = await bcrypt.genSalt(10);
    this.password = await bcrypt.hash(this.password, salt);
-   next();
 });
+
+// basicamente eu crio uma função dentro do meu schema, e eu posso chamar ela quando eu quiser manipular algum dado
+// essa função serve para criar o token do jwt
+UserSchema.methods.createToken = function () {
+   return jwt.sign({ userID: this._id, name: this.name }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME });
+};
+
+// essa função serve para comparar as senhas, ela recebe a senha do body como parametro e é comparada com a senha criptografada, e retorna se deu "match"
+UserSchema.methods.comparePassword = async function(bodyPassword){
+   const match = await bcrypt.compare(bodyPassword, this.password)
+   return match
+}
 
 export default mongoose.model("User", UserSchema);
