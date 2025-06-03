e4d76471303d02ee33c963f4487944b6fab472f0
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-03 19:50:23 -0300

    added auth middleware, admin middleware, and users route

--- src/app.js
+++ src/app.js
@@ -3,13 +3,21 @@ import notFoundMiddleware from "./middlewares/not-found.js";
 import errorHandlerMiddleware from "./middlewares/error-handler.js";
 import auth from "./routes/auth.js";
 import jobs from "./routes/jobs.js";
+import users from "./routes/users.js";
+import authenticationMiddleware from "./middlewares/authentication.js";
+import adminMiddleware from "./middlewares/adminMiddleware.js";
 
 const app = express();
+
+// middleware para ler json no body
 app.use(express.json());
 
+// rotas
 app.use("/api/v1/auth", auth);
-app.use("/api/v1/jobs", jobs);
+app.use("/api/v1/jobs", authenticationMiddleware, jobs);
+app.use("/api/v1/users", authenticationMiddleware, adminMiddleware, users);
 
+// middlewares de erros
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
