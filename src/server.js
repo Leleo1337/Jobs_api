fa27ddcd1a644055222c9f2f836233fcfebc300e
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-03 19:49:47 -0300

    added comments
    Estou comentando o código com o objetivo de reforçar meu entendimento

--- src/server.js
+++ src/server.js
@@ -7,12 +7,18 @@ configDotenv();
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
+// função para iniciar o servidor
 async function start() {
    try {
+      // conecta com o banco de dadoos
       await connectDB(MONGO_URI);
+
+      //se não houver porta no .env, vai dar erro
       if (!PORT) {
          throw new Error("No port found in your .env");
       }
+
+      // vai rodar o servidor
       app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
    } catch (e) {
       console.log(e);
