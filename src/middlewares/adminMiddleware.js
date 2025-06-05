1c093d5717ae53eeee01f5768dd37680e30aa1c5
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-05 15:24:29 -0300

    create adminMiddleware

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,7 +1,14 @@
 // placeholder
 
-function adminMiddleware(req,res,next){
-    next()
+import BadRequest from "../errors/bad-request.js";
+
+function adminMiddleware(req, res, next) {
+   const { isAdmin } = req.user;
+
+   if (!isAdmin) {
+      throw new BadRequest("You have no permission to access this route");
+   }
+   next();
 }
 
-export default adminMiddleware
+export default adminMiddleware;
